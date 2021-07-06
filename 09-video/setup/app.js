2507b8283d7366ed40ebce514b9482bf4420924c
Bretbret18 <bret.sperry@gmail.com>
2021-07-06 18:38:07 -0400

    video on/off switch and preloader

--- 09-video/setup/app.js
+++ 09-video/setup/app.js
@@ -1,3 +1,32 @@
 // MDN
-// The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
-// The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+// The DOMContentLoaded event fires when the initial HTML
+// document has been completely loaded and parsed,
+// without waiting for stylesheets, images, and subframes to
+// finish loading.
+// The load event is fired when the whole page has loaded,
+// including all dependent resources such as stylesheets
+// and images.
+
+const btn = document.querySelector('.switch-btn');
+const video = document.querySelector('.video-container');
+
+//  *** starts / stops video *** //
+// .slide / .switch : classes
+// function: on click, if btn doesn't have class of .slide,
+// add it. if does, than remove
+btn.addEventListener('click', function() {
+    if (!btn.classList.contains('slide')) {
+        btn.classList.add('slide')
+        video.pause();
+    } else {
+        btn.classList.remove('slide');
+        video.play();
+    }
+});
+
+// preLoader
+const preLoader = document.querySelector('.preLoader');
+
+window.addEventListener('load', function() {
+    preLoader.classList.add('hide-preloader')
+});
