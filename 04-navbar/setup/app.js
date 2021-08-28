014eb8adc09eb43d3b4731b747922523460f2a7a
Bretbret18 <bret.sperry@gmail.com>
2021-08-28 13:17:10 -0400

    navbar project

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -1,5 +1,18 @@
-const toggle = document.querySelector('.nav-toggle');
+// classList- shows/gets all classes
+// contains - checks classList for specific class
+// add - add class
+// remove - remove class
+// toggle - toggles class
 
-toggle.addEventListener('click', function() {
-    console.log(toggle);
-})
+const navtoggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+navtoggle.addEventListener('click', function () {
+    if (links.classList.contains('show-links')) {
+        //     links.classList.remove('show-links')
+        //    } else {
+        //     links.classList.add('show-links')
+    }
+    links.classList.toggle('show-links')
+
+});
